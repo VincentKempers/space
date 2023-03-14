26c910491ccd696b6cfa91383e0d5ed224cf8ca3
Vincent Kempers <me@vncn.tk>
2023-03-14 22:21:55 +0100

    small change

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,12 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function About() {
-    return (
-        <>
-            <main className={styles.main}>
-                <h1>About</h1>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <main className={styles.main}>
+        <h1>About</h1>
+        <h3> let's change some things in the about </h3>
+      </main>
+    </>
+  );
 }
