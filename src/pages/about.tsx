2c872a1e5a04ca972ada214b0e34de90bad134a1
Vincent Kempers <me@vncn.tk>
2023-05-05 13:26:15 +0200

    undo about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,11 @@
 import styles from "../styles/Home.module.css";
 
-
 export default function About() {
   return (
     <>
       <main className={styles.main}>
         <h1>About</h1>
-        <h3> let's change some things in the about </h3>
+        <h3> I need to write something here for the about </h3>
       </main>
     </>
   );
